d142ecd69b503642626e3f0c32825a26969783b7
Kishan Kumar Pandey <Kishanpandey6272@gmail.com>
2024-05-19 12:40:45 +0530

    plz commit

---
+++ SeleniumPractice/MouseHoverAndKeyboardMethods.py
@@ -0,0 +1,40 @@
+import time
+
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.chrome.service import service, Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+driver.get("https://magento.softwaretestingboard.com/")
+
+time.sleep(1)
+
+women = driver.find_element(By.XPATH ,"//a[@id='ui-id-4']")
+
+action =ActionChains(driver)
+
+action.move_to_element(women).perform()
+#time.sleep(1)
+
+top = driver.find_element(By.XPATH ,"//a[@id='ui-id-9']")
+
+action.move_to_element(top).perform()
+#time.sleep(1)
+
+jacket=driver.find_element(By.XPATH,"//a[@id='ui-id-11']")
+
+action.move_to_element(jacket).perform()
+
+action.click(jacket).perform()
+
+time.sleep(3)
+
+search=driver.find_element(By.ID,"search")
+action.key_down(Keys.SHIFT).send_keys("Tewst").key_up(Keys.SHIFT).send_keys(Keys.ENTER).perform()
+
+#driver.maximize_window()
--- SeleniumPractice/WaitInSel.py
+++ SeleniumPractice/WaitInSel.py
@@ -1,9 +1,38 @@
+import time
+from telnetlib import EC
+
+#from telnetlib import EC
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import service, Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
-driver.get("https://demo.automationtesting.in/Index.html")
-driver.maximize_window()
+driver.get("https://demo.automationtesting.in/")
+#driver.maximize_window()
+
+driver.find_element(By.ID,"email").send_keys("kishanpandey6272@gmail.com")
+driver.find_element(By.ID,"enterimg").click()
+
+
+# wait for 2 second
+#time.sleep(2)
+
+#wait is of two type Implicitwait and Explicit Wait
+#driver.implicitly_wait(2)
+
+
+wait =WebDriverWait(driver,5)
+wait.until(EC.visibility_of_element_located((By.XPATH,"//input[@placeholder='First Name']"))).send_keys("kishan Kumar")
+
+#driver.find_element(By.XPATH,"//input[@placeholder='First Name`']").send_keys("Kishan")
+
+#driver.implicitly_wait(2)
+
+
+
+#wait =WebDriverWait(driver,5)
+#wait.until(EC)
